b588538b4d2018afb8ba1f8774f0f85c0106d9cc
Danny Connell <sendstufftodanny@gmail.com>
2024-05-15 13:00:58 +0100

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-page>
+    <div>
+      <q-icon
+        name="icon-mat-search"
+        size="xl"
+        class="q-ma-lg"
+      />
+      <q-icon
+        name="icon-fa-500px-brands"
+        size="xl"
+        class="q-ma-lg"
+      />
+      <q-icon
+        name="icon-ion-md-add-circle-outline"
+        size="xl"
+        class="q-ma-lg"
+      />
+      <q-icon
+        name="icon-eva-activity-outline"
+        size="xl"
+        class="q-ma-lg"
+      />
+      <q-icon
+        name="icon-ti-arrow-up"
+        size="xl"
+        class="q-ma-lg"
+      />
+      <q-icon
+        name="icon-la-accessible-icon"
+        size="xl"
+        class="q-ma-lg"
+      />
+      <q-icon
+        name="icon-mat-settings"
+        size="xl"
+        class="q-ma-lg"
+      />
+      <q-icon
+        name="icon-if-phone"
+        size="xl"
+        class="q-ma-lg"
+      />
+      <q-icon
+        name="icon-custom-member"
+        size="xl"
+        class="q-ma-lg"
+      />
+    </div>
+
+    <q-select
+      v-model="model"
+      :options="options"
+      dropdown-icon="icon-mat-arrow_drop_down"
+      label="Filled"
+      style="width: 200px;"
+      class="q-ma-xl"
+      filled
+    />
+
+  </q-page>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      model: ref(null),
+      options: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ]
+    }
+  }
+}
+</script>
